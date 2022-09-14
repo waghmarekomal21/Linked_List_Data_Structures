80922d49c2b2411cf6ca88c157a48af6c21f3c81
Komal <waghmarekomal21@gmail.com>
2022-09-14 20:45:38 +0530

    [Komal] To Create Simple Linked List

---
+++ Linked_List/SimpleLinkedList.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    internal class SimpleLinkedList
+    {
+        Node head;
+        public void Simple(int newData)
+        {
+            Node newNode=new Node(newData);
+
+            if(this.head==null)
+            {
+                this.head=newNode;
+            }
+            else
+            {
+                Node temp = head;
+                while(temp.next!=null)
+                {
+                    temp=temp.next;
+                }
+                temp.next=newNode;
+            }
+
+        }
+        public void Display()
+        {
+            Node temp = this.head;
+            if(temp==null)
+            {
+                Console.WriteLine("LinkedList Is Empty");
+            }
+            while(temp!=null)
+            {
+                Console.WriteLine(temp.data);
+                temp=temp.next;
+            }
+        }
+
+    }
+}
